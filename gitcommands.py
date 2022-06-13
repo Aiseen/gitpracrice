54ce8d74f497fb7ae7752bdeb18a92ec3b98b1ab
Aiseen <aysen.sagynbekov@mail.com>
2022-06-13 16:49:52 +0600

    initial commit.

---
+++ gitcommands.py
@@ -0,0 +1,15 @@
+git init (создается папка .git)
+git remote add name url (вместо name url - htpps/ssh)
+git pull origin master
+git status
+git add name file
+git add .
+git commit (-m 'comment')
+git branch (список веток)
+git branch name branch (создаем ветку)
+git checkout name branch  (переключение)
+git push name branch
+git push origin master
+git reset filename (удаление файла)
+
+
